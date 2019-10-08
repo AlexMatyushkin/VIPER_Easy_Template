181c6187cc3962c46067e6063bb095479bd50f82
Алексей Матюшкин <matyushkin.a.v@sberbank.ru>
2019-10-08 17:39:45 +0300

    First Commit

---
+++ VIPER_Module X.xctemplate/VIPER_Module X.xctemplate/With Generic Functions Fullname/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+/// ___VARIABLE_productName:identifier___ Module Presenter
+class ___VARIABLE_productName:identifier___Presenter {
+    
+    weak var view: ___VARIABLE_productName:identifier___ViewProtocol?
+    private var interactor: ___VARIABLE_productName:identifier___InteractorProtocol
+    private var router: ___VARIABLE_productName:identifier___RouterProtocol
+    
+    init(view: ___VARIABLE_productName:identifier___ViewProtocol) {
+        self.view = view
+        self.interactor = ___VARIABLE_productName:identifier___Interactor()
+        self.router = ___VARIABLE_productName:identifier___Router()
+    }
+}
+
+// MARK: - extending ___VARIABLE_productName:identifier___Presenter to implement it's protocol
+extension ___VARIABLE_productName:identifier___Presenter: ___VARIABLE_productName:identifier___PresenterProtocol {
+    
+}
---
+++ VIPER_Module X.xctemplate/VIPER_Module X.xctemplate/With Generic Functions Fullname/___FILEBASENAME___View.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+/// ___VARIABLE_productName:identifier___ Module View
+class ___VARIABLE_productName:identifier___View: UIViewController {
+    
+ 
+    var presenter: ___VARIABLE_productName:identifier___PresenterProtocol!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter = ___VARIABLE_productName:identifier___Presenter(view: self)
+        
+    }
+    
+}
+
+// MARK: - extending ___VARIABLE_productName:identifier___View to implement it's protocol
+extension ___VARIABLE_productName:identifier___View: ___VARIABLE_productName:identifier___ViewProtocol {
+    
+}
